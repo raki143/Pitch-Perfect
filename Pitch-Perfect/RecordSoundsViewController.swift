8f233c8545bdea2da6770a993ce842dbc1d78cd2
raki143 <geek@Tyfones-MacBook.local>
2015-09-28 23:59:57 +0530

    audio file name changed.

--- Pitch-Perfect/RecordSoundsViewController.swift
+++ Pitch-Perfect/RecordSoundsViewController.swift
@@ -44,10 +44,7 @@ class RecordSoundsViewController: UIViewController {
         
         let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
         
-        let currentDateTime = NSDate()
-        let formatter = NSDateFormatter()
-        formatter.dateFormat = "ddMMyyyy-HHmmss"
-        let recordingName = formatter.stringFromDate(currentDateTime)+".wav"
+        let recordingName = "my_audio.wav"
         let pathArray = [dirPath, recordingName]
         let filePath = NSURL.fileURLWithPathComponents(pathArray)
         
