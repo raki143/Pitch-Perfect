5210d52ac38d8db1879e6beba1c4913fecd9d099
Rakesh Kumar <ace.rakesh@gmail.com>
2015-10-02 01:31:06 +0530

    chipmunk, Darthvader Audio effects are added.

--- PlaySoundsViewController.swift
+++ PlaySoundsViewController.swift
@@ -14,19 +14,17 @@ class PlaySoundsViewController: UIViewController {
     var audioPlayer : AVAudioPlayer!
     var receivedAudio : RecordedAudio!
     
+    var audioEngine : AVAudioEngine!
+    var audioFile : AVAudioFile!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-//        if let filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
-//            let filePathUrl = NSURL.fileURLWithPath(filePath)
-//            
-//        }else{
-//            print("the file path is empty")
-//        }
         
         audioPlayer = try? AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
         audioPlayer.enableRate = true
+        
+        audioEngine = AVAudioEngine()
+        audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -59,4 +57,35 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.stop()
     }
 
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        
+        playAudioWithVariablePitch(1000)
+    }
+    @IBAction func playDathvaderAudio(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
+    
+    func playAudioWithVariablePitch(pitch : Float){
+        
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        let audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        let changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        
+        try! audioEngine.start()
+        audioPlayerNode.play()
+        
+        
+    }
 }
